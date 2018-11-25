24849f22bf1e305c06cf59eed78e0dcca4016a09
FacundoLastra <facundolastra94@gmail.com>
2018-11-25 13:51:17 -0300

    added end-points for the server
    is in development, I can not tested it yet

--- server/index.js
+++ server/index.js
@@ -86,21 +86,31 @@ app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'user_read' }))
 // Set route for OAuth redirect
 app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
 
-// Define a simple template to safely generate HTML with values from user's profile
-var template = handlebars.compile(`
-<html><head><title>Twitch Auth Sample</title></head>
-<table>
-    <tr><th>Access Token</th><td>{{accessToken}}</td></tr>
-    <tr><th>Refresh Token</th><td>{{refreshToken}}</td></tr>
-    <tr><th>Display Name</th><td>{{display_name}}</td></tr>
-    <tr><th>Bio</th><td>{{bio}}</td></tr>
-    <tr><th>Image</th><td>{{logo}}</td></tr>
-</table></html>`);
+
+app.get('auth/islogged:token', function(req, res) {
+  let isLogged=false;
+  if(req.session && req.session.passport && req.session.passport.user) {
+    if( request.params.token === req.session.passport.user.accessToken )
+      isLogged=true;
+  }
+  return res.send(JSON.stringify(isLogged))
+})
+
+app.get('/getloggeduser', function(req, res) {
+  if(req.session && req.session.passport && req.session.passport.user) {
+    return res.send(JSON.stringify(req.session.passport.user));
+  }else
+    return res.status(401).send('Sorry, you not have logged');
+})
+
+
+
+
 
 // If user has an authenticated session, display it, otherwise display link to authenticate
 app.get('/', function (req, res) {
   if(req.session && req.session.passport && req.session.passport.user) {
-    res.send(template(req.session.passport.user));
+    res.redirect('http://localhost:8080/home');
     console.log(req.session.passport.user);
   } else {
     res.send('<html><head><title>Twitch Auth Sample</title></head><a href="/auth/twitch"><img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png"></a></html>');
