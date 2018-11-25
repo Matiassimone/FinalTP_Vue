de3587bd98c1bb82402928cda02b3bc049b59c34
FacundoLastra <facundolastra94@gmail.com>
2018-11-25 19:37:48 -0300

    deleted default vuetify component and minor fix in games

--- app/src/App.vue
+++ app/src/App.vue
@@ -19,12 +19,12 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    
   },
   data() {
     return {
