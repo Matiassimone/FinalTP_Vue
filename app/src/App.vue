e186beb73ba98e92bb86e737060a8cbfcf0ec73b
FacundoLastra <facundolastra94@gmail.com>
2018-11-25 19:20:06 -0300

    navBar and games view

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,51 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-    <input type="date" v-model="fecha" date>
-    <button @click="logout">Logout</button>
-  </div>
+  <v-app>
+    <v-toolbar dark app>
+          <v-flex size=2>      
+          <v-toolbar-title class="headline text-uppercase">
+            <span>Zwitch</span>
+          </v-toolbar-title>
+          </v-flex>
+          <v-flex size=1>
+          <v-btn flat><router-link to="/">Home</router-link></v-btn> 
+          </v-flex>
+          <v-flex size=1>
+          <v-btn flat><router-link to="/about">About</router-link></v-btn> 
+          </v-flex>       
+    </v-toolbar>
+      <router-view/>
+      
+</v-app>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld";
+
 export default {
-  name: 'app',
+  name: "App",
+  components: {
+    HelloWorld
+  },
   data() {
     return {
-      fecha: new Date().toLocaleDateString().replace('/', '-').replace('/', '-')
-    }
-  },
-  methods: {
-    logout(){
-      this.$store.commit('removeToken');
-      this.$router.push('/');
-    }
+      //
+    };
   }
-}
+};
 </script>
-
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
