83ec8aef6fe7bd0a19c779f1d22784cdfedbc1e6
Matias Simone <matiassimone.d@gmail.com>
2018-11-27 12:21:07 -0300

    Fixed VuetifyModules problem an added BigQuote

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify from 'vuetify'
 import 'vuetify/src/stylus/app.styl'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: colors.red.darken1, // #E53935
+    secondary: colors.red.lighten4, // #FFCDD2
+    accent: colors.indigo.base // #3F51B5
+  }
 })
--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,31 +1,33 @@
 <template>
   <v-app class="limit-screen">
-
-    
     <div class="background-video"></div>
     
-      <v-layout align-center justify-center column fill-height >
+      <v-layout align-center justify-center column fill-height v-resize="onResize">
         <v-layout align-center justify-center row>
-
-          
-            <div class="big-quote">
-              <h1>asdnsiugbiudrbgirbiu</h1>
+            <div class="big-quote" >
+              <p class="dimitri-title">"Zwitch the best platform to stream"
+                <svg class="logo-FillColor" overflow="visible" width="30px" height="30px" viewBox="0 0 30 30" x="0px" y="0px">
+                  <g>
+                    <path d="M4,7 L5.56799,3 L27,3 L27,18 L21,24 L16,24 L12.88599,27 L9,27 L9,24 L4,24 L4,7 Z M21,20 L25,16 L25,5 L8,5 L8,20 L12,20 L12,23 L15,20 L21,20 Z"></path>
+                    <g>
+                      <polygon points="21 9 19 9 19 15 21 15"></polygon>
+                      <polygon points="16 9 14 9 14 15 16 15"></polygon>
+                    </g>
+                  </g>
+                </svg>
+              </p>
             </div>
-          
-
         </v-layout>
       </v-layout>
 
-    <v-layout v-resize="onResize">
-     <iframe :src="addImageResolution()" allowfullscreen="true" scrolling="no" :height= "windowSize.y"   :width= "windowSize.x"  ></iframe>
-    </v-layout>
+     <iframe  :src="addImageResolution()" allowfullscreen="true" scrolling="no" :height= "windowSize.y"   :width= "windowSize.x"  ></iframe>
+
   </v-app>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import authService from "../services/AuthService.js";
-
 
 export default {
   name: "login",
@@ -39,16 +41,19 @@ export default {
     return {
       user: "",
       password: "",
+      videoUrl: "",
 
       windowSize: {
         x: 0,
         y: 0
       }
-    };
+    }
   },
 
   computed: {
-    ...mapState('streams', ['topStreams'])
+    ...mapState('streams', ['topStreams']),
+
+
   },
 
   methods: {
@@ -57,17 +62,16 @@ export default {
     },
 
     addImageResolution() {
-      let url = ('https://player.twitch.tv/?channel='+ this.topStreams.data[0].user_name);
-      console.log(url);
-      
-      return url;
+      if(!this.topStreams.loading) {
+        let videoUrl = ('https://player.twitch.tv/?channel='+ this.topStreams.data[0].user_name);
+        return videoUrl
+      }
     },
 
-    onResize () {
+    onResize: function () {
         this.windowSize = { x: window.innerWidth*1, y: window.innerHeight*12 }
-        console.log(this.windowSize);
-        
     }
+
   }
 };
 
@@ -91,6 +95,16 @@ export default {
     background: transparent;
   }
 
+  @font-face {
+    font-family: 'dimitri';
+    src: url("../../fonts/DIMIS___.TTF");
+  }
+
+  .dimitri-title {
+    font-family: 'dimitri';
+    font-size: 8vh;
+  }
+
   .limit-screen{
     height: 100vh;
   }
@@ -108,14 +122,16 @@ export default {
   }
 
   .big-quote{
-    background-color: #6441A4;
+    margin-right: 5vh;
+    margin-left: 5vh;
+    padding-right: 0;
+    padding-left: 0;
+
+    margin-top: 70vh;
+    color: #6441d5;
     z-index: 2;
-    position: absolute;
+    position: auto;
   }
-
-
-
-
 
 </style>
 
