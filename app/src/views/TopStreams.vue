7b651a1c1029c0611a29557495d1d171112db2cf
Matias Simone <matiassimone.d@gmail.com>
2018-11-27 20:10:55 -0300

    Initial commit branch

--- app/src/views/TopStreams.vue
+++ app/src/views/TopStreams.vue
@@ -1,11 +1,52 @@
 <template>
-    
+  <v-content fluid>
+    <v-layout>
+      <v-flex md12 class="default">
+        <v-container grid-list-xl fluid>
+          <v-layout row wrap>
+            <v-flex
+                v-for="stream in streamsByGameId.data"
+                :key="stream.id"
+                md3
+            >
+            <div @click="redirect(stream)" class="pointer">
+              <OneStreamCard :stream=stream />
+            </div>
+            </v-flex>
+          </v-layout>
+        </v-container>  
+      </v-flex>  
+    </v-layout>
+  </v-content>
 </template>
+
 <script>
+import { mapState } from 'vuex'
+import OneStreamCard from '../components/OneStreamCard.vue'
+
 export default {
-    name: 'topstreams'
+    name: 'topstreams',
+
+    components:{
+        OneStreamCard
+    },
+
+    computed: {
+        ...mapState('streams', ['streamsByGameId'])
+    },
+
+    methods: {
+         redirect(stream){ 
+            this.$router.push({ name: "onestream", params: { stream: stream} });
+        } 
+    },
+
+    mounted() {
+    this.$store.dispatch('streams/BEGIN_FETCH_STREAMSBYGAMEID',this.$route.params.gameId);
+  }
 }
 </script>
-<style>
 
+<style>
+    .pointer { cursor: pointer; }
 </style>
